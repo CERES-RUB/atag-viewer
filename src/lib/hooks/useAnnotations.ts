9d11e6210d32d5f49ace9abe8e99fbade90576ab
Rainer Simon <rainer@rainersimon.io>
2024-06-07 13:03:10 +0200

    Bugfix: removed point image annotations

--- src/lib/hooks/useAnnotations.ts
+++ src/lib/hooks/useAnnotations.ts
@@ -10,10 +10,19 @@ export const useAnnotations = <T extends W3CAnnotation = W3CAnnotation>(path: st
       .then((response) => response.json())
       .then(annotations => {
         const filtered = 
-          // Remove annotations without tags
           annotations.filter((a: W3CAnnotation) => {
+            // Remove annotations without tags
             const bodies = Array.isArray(a.body) ? a.body : [a.body];
-            return bodies.some(b => b.purpose === 'tagging');
+            const hasTags = bodies.some(b => b.purpose === 'tagging');
+
+            // Remove image point annotations
+            const target = Array.isArray(a.target) ? a.target[0] : a.target;
+            const selectors = Array.isArray(target.selector) ? target.selector : [target.selector];
+
+            const fragmentSelector = selectors.find(s => s.type === 'FragmentSelector');
+            const isPointAnnotation = fragmentSelector && (fragmentSelector.value as string).endsWith(',0,0');
+
+            return hasTags && !isPointAnnotation;
           });
 
         setAnnotations(filtered);
