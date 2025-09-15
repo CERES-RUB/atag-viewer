6aea12e716c52a22b27e45ffa4abc458ecf0debb
Rainer Simon <rainer@rainersimon.io>
2025-09-15 15:34:47 +0200

    Nav changes

--- src/components/TabbedDocumentIndex/TabbedDocumentIndex.tsx
+++ src/components/TabbedDocumentIndex/TabbedDocumentIndex.tsx
@@ -37,7 +37,7 @@ export const TabbedDocumentIndex = (props: TabbedDocumentIndexProps) => {
       onValueChange={onChangeTab}>
       <Tabs.List className="tabs-list">
         <Tabs.Trigger value="verses">
-          Sanskrit Verses ({verses.length})
+         Textual Works ({verses.length})
         </Tabs.Trigger>
 
         <Tabs.Trigger value="images">
@@ -47,29 +47,42 @@ export const TabbedDocumentIndex = (props: TabbedDocumentIndexProps) => {
 
       <Tabs.Content className="tabs-content" value="verses">
         <ul>
-          <li className="disabled">Aśvaghoṣa: Buddhacarita. Sarga 1.</li>
-          <li className="disabled">Aśvaghoṣa: Buddhacarita. Sarga 2.</li>
-          <li className="disabled">Aśvaghoṣa: Buddhacarita. Sarga 3.</li>
-          <li className="disabled">Aśvaghoṣa: Buddhacarita. Sarga 4.</li>
-					{verses.map(meta => (
-						<li key={meta.slug}>
-							<a href={`verse/${meta.slug}`}>{meta.title}</a>
-              {meta.annotations === 0 && (
-                <span className="work-in-progress">WORK IN PROGRESS</span>
-              )}
-						</li>
-					))}
-          <li className="disabled">Aśvaghoṣa: Buddhacarita. Sarga 6.</li>
-          <li className="disabled">Aśvaghoṣa: Buddhacarita. Sarga 7.</li>
-          <li className="disabled">Aśvaghoṣa: Buddhacarita. Sarga 8.</li>
-          <li className="disabled">Aśvaghoṣa: Buddhacarita. Sarga 9.</li>
-          <li className="disabled">Aśvaghoṣa: Buddhacarita. Sarga 10.</li>
-          <li className="disabled">Aśvaghoṣa: Buddhacarita. Sarga 11.</li>
-          <li className="disabled">Aśvaghoṣa: Buddhacarita. Sarga 12.</li>
-          <li className="disabled">Aśvaghoṣa: Buddhacarita. Sarga 13.</li>
-          <li className="disabled">Aśvaghoṣa: Buddhacarita. Sarga 14.</li>
-          <li className="disabled">Aśvaghoṣa: Buddhacarita. Sarga 15.</li>
-				</ul>
+          <li>
+            <h3>1. Buddhacarita (Life of the Buddha)</h3>
+            <ul>
+              <li className="disabled">Aśvaghoṣa: Buddhacarita. Sarga 1.</li>
+              <li className="disabled">Aśvaghoṣa: Buddhacarita. Sarga 2.</li>
+              <li className="disabled">Aśvaghoṣa: Buddhacarita. Sarga 3.</li>
+              <li className="disabled">Aśvaghoṣa: Buddhacarita. Sarga 4.</li>
+              {verses.map(meta => (
+                <li key={meta.slug}>
+                  <a href={`verse/${meta.slug}`}>{meta.title}</a>
+                  {meta.annotations === 0 && (
+                    <span className="work-in-progress">WORK IN PROGRESS</span>
+                  )}
+                </li>
+              ))}
+              <li className="disabled">Aśvaghoṣa: Buddhacarita. Sarga 6.</li>
+              <li className="disabled">Aśvaghoṣa: Buddhacarita. Sarga 7.</li>
+              <li className="disabled">Aśvaghoṣa: Buddhacarita. Sarga 8.</li>
+              <li className="disabled">Aśvaghoṣa: Buddhacarita. Sarga 9.</li>
+              <li className="disabled">Aśvaghoṣa: Buddhacarita. Sarga 10.</li>
+              <li className="disabled">Aśvaghoṣa: Buddhacarita. Sarga 11.</li>
+              <li className="disabled">Aśvaghoṣa: Buddhacarita. Sarga 12.</li>
+              <li className="disabled">Aśvaghoṣa: Buddhacarita. Sarga 13.</li>
+              <li className="disabled">Aśvaghoṣa: Buddhacarita. Sarga 14.</li>
+              <li className="disabled">Aśvaghoṣa: Buddhacarita. Sarga 15.</li>
+            </ul>
+          </li>
+
+          <li>
+            <h3>2. Saundarananda (Handsome Nanda)</h3>
+          </li>
+
+          <li>
+            <h3>3. Śāriputraprakaraṇa (The Drama of Śāriputra)</h3>
+          </li>
+        </ul>
       </Tabs.Content>
 
       <Tabs.Content className="tabs-content" value="images">
